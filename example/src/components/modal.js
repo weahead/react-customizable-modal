e114e6746fe381425df054561a3ac065e361611c
Jonathan Borg <jonathan@x3m.io>
2019-10-15 23:09:16 +0200

    fix: positioning of modal in overlay

--- example/src/components/modal.js
+++ example/src/components/modal.js
@@ -6,12 +6,15 @@ export default function Modal({ onClose }) {
   return (
     <div
       style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
         position: "fixed",
         top: 0,
         bottom: 0,
         left: 0,
         right: 0,
-        backgroundColor: "#000000"
+        backgroundColor: "rgba(0,0,0,0.6)"
       }}
     >
       <div
