8cc75811736f2d7284eebd4cc9fe9bb961a6f675
Jonathan Borg <jonathan@x3m.io>
2019-10-15 22:56:41 +0200

    feat: add example app

---
+++ example/src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useTrapFocus } from "react-customizable-modal";
+function App() {
+  const trapRef = useTrapFocus();
+  return (
+    <div className="App" ref={trapRef}>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
