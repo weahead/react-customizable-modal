1446939b147d1ea01a244cad76ca409880265895
Jonathan Borg <jonathan@x3m.io>
2019-11-22 00:51:44 +0100

    feat: export useHandleKeyPress hook

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { ModalPortal } from "./containers/ModalPortal";
 export { Modal } from "./containers/Modal";
 export { useAriaHide } from "./hooks/useAriaHide";
 export { useTrapFocus } from "./hooks/useTrapFocus";
+export { useHandleKeyPress } from "./hooks/useHandleKeyPress";
 export { useBodyScrollLock } from "./hooks/useBodyScrollLock";
