903cbffa87ff26c77c9998f355cbf570cc2204dd
Jonathan Borg <jonathan@x3m.io>
2019-11-22 00:50:40 +0100

    feat: add basic modal component

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { useModal } from "./hooks/useModal";
-export { ModalPortal } from "./components/ModalPortal";
+export { ModalPortal } from "./containers/ModalPortal";
+export { Modal } from "./containers/Modal";
 export { useAriaHide } from "./hooks/useAriaHide";
 export { useTrapFocus } from "./hooks/useTrapFocus";
 export { useBodyScrollLock } from "./hooks/useBodyScrollLock";
