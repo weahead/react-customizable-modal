a8c187d22f2f4083acf5a171c7dc0b1928dcb519
Jonathan Borg <jonathan@x3m.io>
2019-12-01 11:20:31 +0100

    feat: add new hook

---
+++ packages/react-customizable-modal/src/hooks/useCloseOnEsc.ts
@@ -0,0 +1,16 @@
+import { useCallback } from 'react';
+import { useHandleKeyPress } from './useHandleKeyPress';
+
+const ESC = 27
+type onEscapeType = Function
+
+export function useCloseOnEsc(onEscape: onEscapeType) {
+    const handleOnEsc = useCallback(event => {
+        const { keyCode } = event;
+        if (keyCode === ESC) {
+            onEscape()
+        }
+    }, [onEscape])
+    useHandleKeyPress(handleOnEsc)
+
+}
