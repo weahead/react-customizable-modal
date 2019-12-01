b60e428d96c7428b83e04b7cc80e133436bfe37c
Jonathan Borg <jonathan@x3m.io>
2019-12-01 11:20:39 +0100

    chore: add todo

--- packages/react-customizable-modal/src/containers/Modal.tsx
+++ packages/react-customizable-modal/src/containers/Modal.tsx
@@ -6,36 +6,50 @@ import { useBodyScrollLock } from "../hooks/useBodyScrollLock";
 import { useTrapFocus } from "../hooks/useTrapFocus";
 import { useHandleKeyPress } from "../hooks/useHandleKeyPress";
 interface Props {
-    id: string;
-    isOpen: boolean;
-    role: string;
-    ariaHideApp: boolean;
-    shouldFocusAfterRender: boolean;
-    onEscape: Function;
-    onOverlayClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
-    shouldReturnFocusAfterClose: boolean;
+  id: string;
+  isOpen: boolean;
+  role: string;
+  ariaHideApp: boolean;
+  shouldFocusAfterRender: boolean;
+  onEscape: Function;
+  onOverlayClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  shouldReturnFocusAfterClose: boolean;
 }
-const ESC = 27
+const ESC = 27;
 export const Modal: React.FC<Props> = ({
-    id = 'modal-root',
-    isOpen = false,
-    role = "dialog",
-    ariaHideApp = true,
-    shouldFocusAfterRender = true,
-    onOverlayClick,
-    shouldReturnFocusAfterClose = true,
-    onEscape,
-    children
+  id = "modal-root",
+  isOpen = false,
+  role = "dialog",
+  ariaHideApp = true,
+  shouldFocusAfterRender = true,
+  onOverlayClick,
+  shouldReturnFocusAfterClose = true,
+  onEscape,
+  children
 }) => {
-
-    const handleOnEsc = useCallback(event => {
-        const { keyCode } = event;
-        if (isOpen && keyCode === ESC) {
-            onEscape()
-        }
-    }, [onEscape])
-    useHandleKeyPress(handleOnEsc)
-    useBodyScrollLock()
-    const modalRef = useTrapFocus({ focusOnRender: shouldFocusAfterRender, returnFocus: shouldReturnFocusAfterClose })
-    return open ? <ModalPortal id={id}><Overlay onClick={onOverlayClick}><UIModal role={role} ref={modalRef}>{children}</UIModal></Overlay></ModalPortal> : null;
+  //TODO: use useCloseOnEsc
+  const handleOnEsc = useCallback(
+    event => {
+      const { keyCode } = event;
+      if (isOpen && keyCode === ESC) {
+        onEscape();
+      }
+    },
+    [onEscape]
+  );
+  useHandleKeyPress(handleOnEsc);
+  useBodyScrollLock();
+  const modalRef = useTrapFocus({
+    focusOnRender: shouldFocusAfterRender,
+    returnFocus: shouldReturnFocusAfterClose
+  });
+  return open ? (
+    <ModalPortal id={id}>
+      <Overlay onClick={onOverlayClick}>
+        <UIModal role={role} ref={modalRef}>
+          {children}
+        </UIModal>
+      </Overlay>
+    </ModalPortal>
+  ) : null;
 };
