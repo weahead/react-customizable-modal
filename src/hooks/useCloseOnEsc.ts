97e87eaa697151516e79c6e0b05573ed42b259bb
Jonathan Borg <jonathan@x3m.io>
2020-03-21 22:05:45 +0100

    fix: formatting

--- src/hooks/useCloseOnEsc.ts
+++ src/hooks/useCloseOnEsc.ts
@@ -1,16 +1,18 @@
 import { useCallback } from 'react';
 import { useHandleKeyPress } from './useHandleKeyPress';
 
-const ESC = 27
-type onEscapeType = Function
+const ESC = 27;
+type onEscapeType = Function;
 
 export function useCloseOnEsc(onEscape: onEscapeType) {
-    const handleOnEsc = useCallback(event => {
-        const { keyCode } = event;
-        if (keyCode === ESC) {
-            onEscape()
-        }
-    }, [onEscape])
-    useHandleKeyPress(handleOnEsc)
-
+  const handleOnEsc = useCallback(
+    event => {
+      const { keyCode } = event;
+      if (keyCode === ESC) {
+        onEscape();
+      }
+    },
+    [onEscape]
+  );
+  useHandleKeyPress(handleOnEsc);
 }
