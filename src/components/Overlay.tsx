a31ef190f83a9bfde7a91c1b27c69268884b8e96
Jonathan Borg <jonathan@x3m.io>
2020-03-22 19:06:24 +0100

    fix: closes #5

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,18 +1,18 @@
-import React from "react";
-interface Props {
-    onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
-}
+import React from 'react';
 
-export const Overlay: React.FC<Props> = ({
-    onClick,
-    children
-}) => {
-    return <div onClick={onClick} style={{
-        position: "fixed",
+export const Overlay: React.FC = ({ children }) => {
+  return (
+    <div
+      style={{
+        position: 'fixed',
         top: 0,
         left: 0,
         right: 0,
         bottom: 0,
-        backgroundColor: "rgba(255, 255, 255, 0.75)"
-    }}>{children}</div>;
+        backgroundColor: 'rgba(255, 255, 255, 0.75)',
+      }}
+    >
+      {children}
+    </div>
+  );
 };
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { useCloseOnEsc } from "./hooks/useCloseOnEsc";
 export { useTrapFocus } from "./hooks/useTrapFocus";
 export { useHandleKeyPress } from "./hooks/useHandleKeyPress";
 export { useBodyScrollLock } from "./hooks/useBodyScrollLock";
+export { useOnClickOutside } from "./hooks/useOnClickOutside";
